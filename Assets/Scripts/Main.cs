ed1f12a350747eee97cc0868b39b2080b6e9ec2a
scx567888 <scx567888@outlook.com>
2025-08-08 14:50:52 +0800

    dev

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,81 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Enums;
+using DefaultNamespace;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
+/// <summary>
+/// 这是一个桥接类
+/// </summary>
 public class Main : MonoBehaviour
 {
-    private GameObject ground;
-    private GameObject redBossPrefab;
-    private GameObject redSoldierPrefab;
-    private GameObject blueBossPrefab;
-    private GameObject blueSoldierPrefab;
+    private Game game;
 
     void Awake()
     {
-        // 初始化 4个预制体
-        redBossPrefab = Resources.Load<GameObject>("Prefabs/RedBoss");
-        redSoldierPrefab = Resources.Load<GameObject>("Prefabs/RedSoldier");
-        blueBossPrefab = Resources.Load<GameObject>("Prefabs/BlueBoss");
-        blueSoldierPrefab = Resources.Load<GameObject>("Prefabs/BlueSoldier");
-        // 获取地板
-        ground = GameObject.Find("Ground");
+        ViewFactory.init();
+        game = new Game();
     }
 
-    private void InitBoss()
-    {
-        // 1. 获取地板的位置
-        Vector3 groundPos = ground.transform.position;
-
-        // 2. 获取地板的宽度（假设宽度是 x 轴方向）
-        Renderer groundRenderer = ground.GetComponent<Renderer>();
-        float groundWidth = groundRenderer.bounds.size.x;
-
-        // 3. 计算左右两侧的位置（以地板中心为基准）
-        // 这里偏移量可以根据需要调整，比如偏离地板边缘一点
-        float offset = groundWidth / 2 - 2f; // 2f是额外距离
-
-        Vector3 leftPos = new Vector3(groundPos.x - offset, groundPos.y, groundPos.z);
-        Vector3 rightPos = new Vector3(groundPos.x + offset, groundPos.y, groundPos.z);
-
-        // 4. 生成红色Boss在左边，蓝色Boss在右边
-        var redBoss = Instantiate(redBossPrefab, leftPos, Quaternion.identity);
-        var blueBoss = Instantiate(blueBossPrefab, rightPos, Quaternion.identity);
-        // 5. 设置名字和阵营
-        redBoss.name = "RedBoss";
-        redBoss.GetComponent<Boss>().camp = Camp.RED;
-        blueBoss.name = "BlueBoss";
-        blueBoss.GetComponent<Boss>().camp = Camp.BLUE;
-    }
-
-    private void InitSoldiers()
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            Vector3 randomOffset = new Vector3(
-                Random.Range(-3f, 3f),  // x 方向随机偏移
-                0f,                     // y 不偏移（保持在地面上）
-                Random.Range(-3f, 3f)   // z 方向随机偏移
-            );
-
-            Vector3 spawnPos = Vector3.zero + randomOffset;
-            Instantiate(blueSoldierPrefab, spawnPos, Quaternion.identity);    
-        }
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        InitBoss();
-        InitSoldiers();
+        game.Start();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        game.Update();
     }
     
 }
